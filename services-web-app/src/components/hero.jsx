06c8c8719fa5236aad0a322af36968c0cf086ce2
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-12-23 18:23:24 -0700

    added hero section

---
+++ services-web-app/src/components/hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="hero-area">
+      <div className="hero-container">
+        <div className="hero-intro">
+          <h2>Subic Bay Home Services</h2>
+          <p>Providing quality home services in the Subic Bay area</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Hero;
