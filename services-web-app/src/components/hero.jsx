d47d0e00476ccfe003ec9d1509d7273d121f5e5a
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-12-23 18:53:17 -0700

    styles folder strucute change and nav bar style fix

--- services-web-app/src/components/hero.jsx
+++ services-web-app/src/components/hero.jsx
@@ -1,14 +1,17 @@
+import '../styles/hero.css';
+
 const Hero = () => {
-  return (
-    <div className="hero-area">
-      <div className="hero-container">
-        <div className="hero-intro">
-          <h2>Subic Bay Home Services</h2>
-          <p>Providing quality home services in the Subic Bay area</p>
+    return (
+        <div className="hero-area">
+            <div className="hero-container">
+                <div className="hero-intro">
+                    <h2>Reliable Home Services, Book in Minutes</h2>
+                    <p>Connect with trusted handymen and cleaning professionals through one simple, secure platform.
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 
--- services-web-app/src/components/navbar.jsx
+++ services-web-app/src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/navbar.css';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
