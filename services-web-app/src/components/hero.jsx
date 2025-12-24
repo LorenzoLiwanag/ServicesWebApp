d6f0b2403ec50f68b887eb32b983968687620403
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-12-23 19:18:26 -0700

    styling hero section

--- services-web-app/src/components/hero.jsx
+++ services-web-app/src/components/hero.jsx
@@ -1,21 +1,29 @@
-import '../styles/hero.css';
+import "../styles/hero.css";
 
 const Hero = () => {
-    return (
-        <div className="hero-area">
-            <div className="hero-container">
-                <div className="hero-intro">
-                    <h2>Reliable Home Services, Book in Minutes</h2>
-                    <p>Connect with trusted handymen and cleaning professionals through one simple, secure platform.
-                    </p>
-                <div classname = "hero-image">
-                    <img src={require('../assets/hero-image.jpg')} alt="Hero" />
-                </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+  return (
+    <section className="hero-area">
+      <div className="hero-container">
+        <div className="hero-content">
+          <div className="hero-intro">
+            <h2>Reliable Home Services, Book in Minutes</h2>
+            <p>
+              Connect with trusted handymen and cleaning professionals through one
+              simple, secure platform.
+            </p>
+          </div>
 
+          <div className="hero-image">
+            <img
+              src={require("../assets/hero-image.jpg")}
+              alt="Hero"
+              className="hero-img"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
