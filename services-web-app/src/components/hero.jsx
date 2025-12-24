783d28df9a2e4ec14836c3c9b0773a7a1414fdcf
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-12-23 19:04:47 -0700

    hero section edits

--- services-web-app/src/components/hero.jsx
+++ services-web-app/src/components/hero.jsx
@@ -8,6 +8,9 @@ const Hero = () => {
                     <h2>Reliable Home Services, Book in Minutes</h2>
                     <p>Connect with trusted handymen and cleaning professionals through one simple, secure platform.
                     </p>
+                <div classname = "hero-image">
+                    <img src={require('../assets/hero-image.jpg')} alt="Hero" />
+                </div>
                 </div>
             </div>
         </div>
