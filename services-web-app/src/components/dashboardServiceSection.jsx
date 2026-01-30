079e0c65f1264b6c3d62af5d5fae49f4b559c18e
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2026-01-30 16:53:02 -0700

    added services section to the client dashboard

---
+++ services-web-app/src/components/dashboardServiceSection.jsx
@@ -0,0 +1,9 @@
+const DashboardServiceSection = () => {
+    return (
+        <div>
+            <h2>Recommended Services</h2>
+        </div>
+    );
+}
+
+export default DashboardServiceSection;
--- services-web-app/src/pages/ClientDashboardPage.jsx
+++ services-web-app/src/pages/ClientDashboardPage.jsx
@@ -1,10 +1,12 @@
 import DashboardNavbar from "../components/DashboardNavbar";
 import DashboardSearch from "../components/dashboardSearch";
+import DashboardServiceSection from "../components/dashboardServiceSection";
 const ClientDashboard = () => { 
     return (
         <div>
             <DashboardNavbar /> 
             <DashboardSearch /> 
+            <DashboardServiceSection /> 
             {/* <h1>Welcome Back User Name!</h1> */}
         </div>
     );
