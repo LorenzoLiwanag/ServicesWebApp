f2ec067555ee31edabb5652455b10864ec1cb52e
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2026-02-10 19:54:50 -0700

    edited dashboard style

--- services-web-app/src/pages/ClientDashboardPage.jsx
+++ services-web-app/src/pages/ClientDashboardPage.jsx
@@ -1,16 +1,22 @@
 import DashboardNavbar from "../components/DashboardNavbar";
 import DashboardSearch from "../components/dashboardSearch";
 import DashboardServiceSection from "../components/dashboardServiceSection";
-import DashboardMyBookings from "../components/dashboardMyBookings";    
+import DashboardMyBookings from "../components/dashboardMyBookings";
+import "../styles/clientDashboard.css";    
 const ClientDashboard = () => { 
     return (
         <div>
             <DashboardNavbar /> 
-            <h1>Welcome Back User!</h1>
+            <h1 className="welcome-heading">Welcome Back User!</h1>
             <DashboardSearch /> 
-            <DashboardServiceSection /> 
-            <DashboardMyBookings />
-
+            <div className="dashboard-content-wrapper">
+                <div className="dashboard-services-column">
+                    <DashboardServiceSection /> 
+                </div>
+                <div className="dashboard-bookings-column">
+                    <DashboardMyBookings />
+                </div>
+            </div>
         </div>
     );
 }   
