27a708a07dfeffcdd147d47e236a83682060abcc
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2026-03-24 18:38:22 -0600

    Revert "handle logout"
    This reverts commit 0c7417921b8e5da54ec26f2748e4da3dfa1be5d8.

--- services-web-app/src/pages/ClientDashboardPage.jsx
+++ services-web-app/src/pages/ClientDashboardPage.jsx
@@ -3,38 +3,31 @@ import DashboardSearch from "../components/dashboard/DashboardSearch";
 import DashboardServiceSection from "../components/dashboard/DashboardServiceSection";
 import DashboardMyBookings from "../components/dashboard/DashboardMyBookings";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "../styles/dashboard/clientDashboard.css";
-
 const ClientDashboard = () => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        setUser(storedUser);
+    }, []);
 
-    if (!storedUser) {
-      navigate("/login");
-    } else {
-      setUser(storedUser);
-    }
-  }, [navigate]);
 
-  return (
-    <div>
-      <DashboardNavbar />
-      {user ? `Welcome back, ${user.fullName}` : "Welcome"}
-      <DashboardSearch />
-      <div className="dashboard-content-wrapper">
-        <div className="dashboard-services-column">
-          <DashboardServiceSection />
+    return (
+        <div>
+            <DashboardNavbar />
+            {user ? `Welcome back, ${user.fullName}` : "Welcome"}
+            <DashboardSearch />
+            <div className="dashboard-content-wrapper">
+                <div className="dashboard-services-column">
+                    <DashboardServiceSection />
+                </div>
+                <div className="dashboard-bookings-column">
+                    <DashboardMyBookings />
+                </div>
+            </div>
         </div>
-        <div className="dashboard-bookings-column">
-          <DashboardMyBookings />
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default ClientDashboard;
